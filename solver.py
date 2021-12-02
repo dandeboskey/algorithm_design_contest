6b13ca52ac1bd1346af11069eaf0f70837593313
Stephen Wang <swangbw@berkeley.edu>
2021-12-01 22:36:33 -0800

    Progress

--- solver.py
+++ solver.py
@@ -10,29 +10,39 @@ def solve(tasks):
     Returns:
         output: list of igloos in order of polishing  
     """
-    print(tasks[0])
+    tasks = [(tasks[i].task_id, tasks[i].deadline, tasks[i].duration, tasks[i].perfect_benefit) for i in range(len(tasks))]
     tasks.sort(key = lambda x: x[0])
-    dp = [[0 for _ in range(len(tasks))] for _ in range(EOD)]
-    for i in range(1, EOD + 1):
-        for t in tasks:
-            deadline, duration, profit = t[0], t[1], t[2]
-            t_latest = min(deadline, EOD) - duration
+    tasks = [0] + tasks
+    dp = [[0 for _ in range(EOD)] for _ in range(len(tasks))]
+    scheduledTasks = []
+    scheduled = set()
+    for t in range(1, EOD):
+        for i in range(1, len(tasks)):
+            task = tasks[i]
+            id, deadline, duration, profit = task[0], task[1], task[2], task[3]
+            if id in scheduled:
+                continue
+            t_latest = min(deadline, t) - duration
             if t_latest < 0:
                 dp[i][t] = dp[i-1][t]
             else:
-                dp[i][t] = max(dp[i-1][t], profit + dp[i-1][t_latest])
-                
-    return dp[-1][-1]
+                if dp[i-1][t] > profit + dp[i-1][t_latest]:
+                    dp[i][t] = dp[i-1][t]
+                else:
+                    dp[i][t] = profit + dp[i-1][t_latest]
+                    scheduledTasks.append(id)
+                    scheduled.add(id)
+    return scheduledTasks
 
 def profitDecay(p, s):
     return p*math.e^(-0.017*s)
 
 def main():
     if __name__ == '__main__':
-        for input_path in os.listdir('inputs/small/'):
+        for input_path in os.listdir('inputs/'):
             print(input_path)
             output_path = 'outputs/' + input_path[:-3] + '.out'
-            tasks = read_input_file('inputs/small/' + input_path)
+            tasks = read_input_file('inputs/' + input_path)
             output = solve(tasks)
             write_output_file(output_path, output)
 
